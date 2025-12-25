e3fb30daa6b4d0a62c656393f11e445df323856a
sebastianvillaf <sebastianvillanuevaflores@gmail.com>
2025-12-25 14:39:25 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,25 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+
+predata=pd.read_csv(r"C:/Users/aaron/Downloads/hola.csv")
+
+z=predata.isna()
+data=predata.fillna(value="")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
