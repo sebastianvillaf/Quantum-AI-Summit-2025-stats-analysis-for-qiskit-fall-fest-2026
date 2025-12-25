fe8cfa9a63d7ad2413b56d3977d09967a11fbbbb
sebastianvillaf <sebastianvillanuevaflores@gmail.com>
2025-12-25 14:42:41 -0500

    Update data.py
    simple count of how many people came each day

--- data.py
+++ data.py
@@ -8,6 +8,13 @@ predata=pd.read_csv(r"C:/Users/aaron/Downloads/hola.csv")
 z=predata.isna()
 data=predata.fillna(value="")
 
+day1=(data["Asistencia día 1"]=="VERDADERO").sum()
+day2=(data["Asistencia día 2"]=="VERDADERO").sum()
+day3=(data["Asistencia día 3"]=="VERDADERO").sum()
+
+print(f"asistieron {day1} personas el Viernes 19")
+print(f"asistieron {day2} personas el Sabado 20")
+print(f"asistieron {day3} personas el Domingo 21")
 
 
 
